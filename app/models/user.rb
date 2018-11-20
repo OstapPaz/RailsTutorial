6d91523aeff76c91f079197d6325e0b2255f9370
OstapPaz <pazynuyk.ostap@gmail.com>
2018-11-20 12:15:57 +0200

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,28 @@
 class User < ApplicationRecord
 
+
+
   has_many :microposts, dependent: :destroy
+
+
+
+
+  has_many :active_relationships, class_name: "Relationship",
+                                              foreign_key: "follower_id",
+                                              dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+
+
+
+
+  has_many :passive_relationships, class_name:  "Relationship",
+           foreign_key: "followed_id",
+           dependent:   :destroy
+  has_many :followers, through: :passive_relationships, source: :follower
+
+
+
+
 
   attr_accessor :remember_token, :activation_token, :reset_token
 
@@ -98,7 +120,26 @@ class User < ApplicationRecord
 
 
   def feed
-    Micropost.where("user_id = ?", id)
+    Micropost.where("user_id IN (:following_ids) OR user_id = :user_id",
+                    following_ids: self.following_ids, user_id: self.id)
+  end
+
+
+
+  def follow(other_user)
+    following << other_user
+  end
+
+
+
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+
+
+
+  def following?(other_user)
+    following.include?(other_user)
   end
 
 
